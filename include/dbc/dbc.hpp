eba5bf28a40ea9aafc48794e6b00b65f1827793d
SoultatosStefanos <stefanoss1498@gmail.com>
2022-03-14 18:47:35 +0200

    Refactoring to introduce different contract handling options, depending on the build type

--- include/dbc/dbc.hpp
+++ include/dbc/dbc.hpp
@@ -12,82 +12,88 @@
 
 #pragma once
 
+#include "details/dbc_private.hpp"
 #include <iostream>
 
-#ifdef NDEBUG
+#if defined(DBC_ABORT)
 
-#define DBC_INVARIANT1(condition)              (void(0))
-#define DBC_INVARIANT2(condition, message)     (void(0))
+#define DBC_ASSERT1(type, condition)                                           \
+    dbc::details::abort({type, #condition, __FUNCTION__, __FILE__, __LINE__});
 
-#define DBC_PRECONDITION1(condition)           (void(0))
-#define DBC_PRECONDITION2(condition, message)  (void(0))
+#define DBC_ASSERT2(type, condition, message)                                  \
+    dbc::details::abort(                                                       \
+        {type, #condition, __FUNCTION__, __FILE__, __LINE__, message});
 
-#define DBC_POSTCONDITION1(condition)          (void(0))
-#define DBC_POSTCONDITION2(condition, message) (void(0))
+#elif defined(DBC_TERMINATE)
+
+#define DBC_ASSERT1(type, condition)                                           \
+    dbc::details::terminate(                                                   \
+        {type, #condition, __FUNCTION__, __FILE__, __LINE__});
+
+#define DBC_ASSERT2(type, condition, message)                                  \
+    dbc::details::terminate(                                                   \
+        {type, #condition, __FUNCTION__, __FILE__, __LINE__, message});
+
+#elif defined(DBC_THROW)
+
+#define DBC_ASSERT1(type, condition)                                           \
+    dbc::details::raise({type, #condition, __FUNCTION__, __FILE__, __LINE__});
+
+#define DBC_ASSERT2(type, condition, message)                                  \
+    dbc::details::raise(                                                       \
+        {type, #condition, __FUNCTION__, __FILE__, __LINE__, message});
+
+#elif defined(DBC_NOTHROW)
+
+#define DBC_ASSERT1(type, condition)          (void(0))
+#define DBC_ASSERT2(type, condition, message) (void(0))
 
 #else
 
-#define DBC_FAIL(context, condition, message)                                  \
-    do {                                                                       \
-        std::cerr << context << " (" << #condition << "), "                    \
-                  << "function: " << __FUNCTION__ << ", file: " << __FILE__    \
-                  << ", line: " << __LINE__ << "." << '\n'                     \
-                  << message << '\n';                                          \
-        std::terminate();                                                      \
-    }                                                                          \
-    while(0)
-
-#define DBC_INVARIANT1(condition)                                              \
-    do {                                                                       \
-        if(!(condition)) DBC_FAIL("Invariant violation:", condition, "");      \
-    }                                                                          \
-    while(0)
-
-#define DBC_INVARIANT2(condition, message)                                     \
-    do {                                                                       \
-        if(!(condition)) DBC_FAIL("Invariant violation:", condition, message); \
-    }                                                                          \
-    while(0)
-
-#define DBC_PRECONDITION1(condition)                                           \
-    do {                                                                       \
-        if(!(condition)) DBC_FAIL("Precondition violation:", condition, "");   \
-    }                                                                          \
-    while(0)
-
-#define DBC_PRECONDITION2(condition, message)                                  \
-    do {                                                                       \
-        if(!(condition))                                                       \
-            DBC_FAIL("Precondition violation:", condition, message);           \
-    }                                                                          \
-    while(0)
-
-#define DBC_POSTCONDITION1(condition)                                          \
-    do {                                                                       \
-        if(!(condition)) DBC_FAIL("Postcondition violation:", condition, "");  \
-    }                                                                          \
-    while(0)
-
-#define DBC_POSTCONDITION2(condition, message)                                 \
-    do {                                                                       \
-        if(!(condition))                                                       \
-            DBC_FAIL("Postcondition violation:", condition, message);          \
-    }                                                                          \
-    while(0)
+#define DBC_ASSERT1(type, condition)          (void(0))
+#define DBC_ASSERT2(type, condition, message) (void(0))
 
 #endif
 
+#define DBC_INVARIANT1(condition)                                              \
+    DBC_ASSERT1("Invariant violation: ", condition)
+#define DBC_INVARIANT2(condition, message)                                     \
+    DBC_ASSERT2("Invariant violation: ", condition, message)
+
+#define DBC_PRECONDITION1(condition)                                           \
+    DBC_ASSERT1("Precondition violation: ", condition)
+#define DBC_PRECONDITION2(condition, message)                                  \
+    DBC_ASSERT2("Precondition violation: ", condition, message)
+
+#define DBC_POSTCONDITION1(condition)                                          \
+    DBC_ASSERT1("Postcondition violation: ", condition)
+#define DBC_POSTCONDITION1(condition, message)                                 \
+    DBC_ASSERT2("Postcondition violation: ", condition, message)
+
+// litle trick to emulate function overloading via preprocessing magic
 #define DBC_EXPAND(x)                    x
 #define DBC_GET_MACRO(_1, _2, NAME, ...) NAME
 
+/**
+ * @brief Macro to assert a class/loop invariant.
+ *
+ */
 #define INVARIANT(...)                                                         \
     DBC_EXPAND(DBC_GET_MACRO(__VA_ARGS__, DBC_INVARIANT2,                      \
                              DBC_INVARIANT1)(__VA_ARGS__))
 
+/**
+ * @brief Macro to assert a precondition.
+ *
+ */
 #define PRECONDITION(...)                                                      \
     DBC_EXPAND(DBC_GET_MACRO(__VA_ARGS__, DBC_PRECONDITION2,                   \
                              DBC_PRECONDITION1)(__VA_ARGS__))
 
+/**
+ * @brief Macro to assert a postcondition.
+ *
+ */
 #define POSTCONDITION(...)                                                     \
     DBC_EXPAND(DBC_GET_MACRO(__VA_ARGS__, DBC_POSTCONDITION2,                  \
                              DBC_POSTCONDITION1)(__VA_ARGS__))
