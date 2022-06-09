e609cd4a216e820f0164283c89a8870b7def8011
SoultatosStefanos <stefanoss1498@gmail.com>
2022-06-09 13:18:00 +0300

    Refactored into a header-only version

--- docs/html/search/functions_4.js
+++ docs/html/search/functions_4.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['throw_5fhandler_19',['throw_handler',['../group__error__handling.html#gaefd993125ae87d0ed1d16a1be978e6fb',1,'dbc']]],
-  ['to_5fstring_5fview_20',['to_string_view',['../group__error__reporting.html#ga50e2d3f289ff17ebfd01057cd2060741',1,'dbc']]]
+  ['throw_5fhandler_19',['throw_handler',['../group__error__handling.html#ga06e0eebf2d3c3296cb44a2561983c896',1,'dbc']]],
+  ['to_5fstring_5fview_20',['to_string_view',['../group__error__reporting.html#gaea025b2a9faec246fd9cbf8622625ccc',1,'dbc']]]
 ];
